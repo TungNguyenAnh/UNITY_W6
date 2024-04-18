27d054f36471ef50ddb14f79c61de4e0ad38172c
TungNguyenAnh <114374972+TungNguyenAnh@users.noreply.github.com>
2024-04-18 18:23:27 +0700

    Create th1.cs

---
+++ th1.cs
@@ -0,0 +1,45 @@
+﻿public class Rectangle
+{
+    double width, height;
+
+    public Rectangle() { }
+
+    public Rectangle(double width, double height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+    public double getArea()
+    {
+        return width * height;
+    }
+
+    public double getPerimeter()
+    {
+        return 2 * width + 2 * height;
+    }
+
+    public string display()
+    {
+        return "width: " + width + " height: " + height;
+    }
+}
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("Enter the width: ");
+        double width = double.Parse(Console.ReadLine());
+
+        System.Console.WriteLine("Enter the height: ");
+        double height = double.Parse(Console.ReadLine());
+
+        Rectangle rectangle = new Rectangle(width, height);
+
+        System.Console.WriteLine("Your rectangle is: " + rectangle.display());
+        System.Console.WriteLine("Perimeter: " + rectangle.getPerimeter());
+        System.Console.WriteLine("Area of the rectangle: " + rectangle.getArea());
+    }
+}
